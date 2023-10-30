f5e5a6a427cc1063d0243e9f1f63500e123fa242
Deadman <47807486+Deaadman@users.noreply.github.com>
2023-10-30 14:42:38 +1030

    two new features (from other modders)
    - added disable breath effects
    - added non potable toilet water
    - need to restructure projects format, quite messy

---
+++ VisualStudio/Tweaks/TolietTweaks.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversalTweaks.Tweaks;
+internal class TolietTweaks
+{
+    [HarmonyPatch(typeof(WaterSource), nameof(WaterSource.Update))] // Has to be update for some reason? Awake or Start don't set all (only a few) toilets to the quality NonPotable.
+    private static class ToiletNonPotableWater
+    {
+        private static void Postfix(WaterSource __instance)
+        {
+            __instance.m_CurrentLiquidQuality = LiquidQuality.NonPotable;
+        }
+    }
+}
