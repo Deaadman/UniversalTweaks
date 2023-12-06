c27a48fb575cd1a4b169b5fcd641c3074780a1cd
Deadman <47807486+Deaadman@users.noreply.github.com>
2023-12-06 21:34:33 +1030

    heavily simplified breath effects logic

--- VisualStudio/TweaksBreath.cs
+++ VisualStudio/TweaksBreath.cs
@@ -7,34 +7,9 @@ internal class TweaksBreath
     [HarmonyPatch(typeof(Breath), nameof(Breath.PlayBreathEffect))]
     private static class DisableBreathEffect
     {
-        private static float originalColdBreathTempThreshold;
-        private static float originalVeryColdBreathTempThreshold;
-        private static float originalFreezingBreathTempThreshold;
-        private static bool hasStoredOriginalValues = false;
-
         private static void Postfix(Breath __instance)
         {
-            if (!hasStoredOriginalValues)
-            {
-                originalColdBreathTempThreshold = __instance.m_ColdBreathTempThreshold;
-                originalVeryColdBreathTempThreshold = __instance.m_VeryColdBreathTempThreshold;
-                originalFreezingBreathTempThreshold = __instance.m_FreezingBreathTempThreshold;
-                hasStoredOriginalValues = true;
-            }
-
-            if (!Settings.Instance.DisableBreathEffect)
-            {
-                __instance.m_ColdBreathTempThreshold = -float.MaxValue;
-                __instance.m_VeryColdBreathTempThreshold = -float.MaxValue;
-                __instance.m_FreezingBreathTempThreshold = -float.MaxValue;
-                __instance.StopBreathEffectImmediate();
-            }
-            else
-            {
-                __instance.m_ColdBreathTempThreshold = originalColdBreathTempThreshold;
-                __instance.m_VeryColdBreathTempThreshold = originalVeryColdBreathTempThreshold;
-                __instance.m_FreezingBreathTempThreshold = originalFreezingBreathTempThreshold;
-            }
+            __instance.m_SuppressEffects = !Settings.Instance.DisableBreathEffect;
         }
     }
 }
