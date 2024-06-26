3a9329efb0345eaa27b0d4a6b854e09c459ec758
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-06-26 16:23:22 +0930

    Updated to TLD v2.29 and ML v0.6.4

--- VisualStudio/Properties/BuildInfo.cs
+++ VisualStudio/Properties/BuildInfo.cs
@@ -8,13 +8,13 @@ internal static class BuildInfo
     /// <summary>Who made the mod</summary>
     public const string Author                          = "Deadman";
     /// <summary>Current version (e.g. 1.0.0, 1.0.0-alpha, 1.0.0-beta, 1.0.0-rc, etc.)</summary>
-    public const string Version                         = "1.4.1";
+    public const string Version                         = "1.4.2-DeveloperBuild";
     /// <summary>Version shown in DnSpy</summary>
-    public const string AssemblyVersion                 = "1.4.1.0";
+    public const string AssemblyVersion                 = "1.4.2.0";
     /// <summary>Name used on GUI's, like ModSettings</summary>
     public const string GUIName                         = "Universal Tweaks";
     /// <summary>The minimum Melon Loader version that your mod requires.</summary>
-    public const string MelonLoaderVersion              = "0.6.1";
+    public const string MelonLoaderVersion              = "0.6.4";
     #endregion
 
     #region Optional
