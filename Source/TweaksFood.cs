37c4029d900cc675716d02ceedea927979f5994f
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-08-11 09:39:13 +0930

    Fixed an issue with fatigue for stews not reducing

--- Source/TweaksFood.cs
+++ Source/TweaksFood.cs
@@ -23,16 +23,16 @@ internal class TweaksFood
             {
                 ComponentUtilities.RestoreComponent<CausesHeadacheDebuff>("GEAR_CookedPiePeach", "GEAR_CookedPieRoseHip", "GEAR_CookedPorridgeFruit");
             }
-
-            if (__instance.gameObject.name == "GEAR_CookedStewMeat" || __instance.gameObject.name == "GEAR_CookedStewVegetables")
+            
+            if (__instance.gameObject.name is "GEAR_CookedStewMeat" or "GEAR_CookedStewVegetables")
             {
-                __instance.m_FoodItem.gameObject.GetComponentInParent<FoodStatEffect>().m_Effect = Settings.Instance.ReduceStewFatigueLossAmount;
+                __instance.gameObject.GetComponentInParent<FoodStatEffect>().m_Effect = Settings.Instance.ReduceStewFatigueLossAmount;
             }
-
-            string[] InsulatedFlasks = ["GEAR_InsulatedFlask_A", "GEAR_InsulatedFlask_B", "GEAR_InsulatedFlask_C", "GEAR_InsulatedFlask_D", "GEAR_InsulatedFlask_E", "GEAR_InsulatedFlask_F"];
+            
+            string[] insulatedFlasks = ["GEAR_InsulatedFlask_A", "GEAR_InsulatedFlask_B", "GEAR_InsulatedFlask_C", "GEAR_InsulatedFlask_D", "GEAR_InsulatedFlask_E", "GEAR_InsulatedFlask_F"];
 
             // When optimising this mod, consider using the GearItem.LoadPrefab blah blah blah - so we can make a separate 'RefreshGearItems' method that can be called when the settings are changed.
-            if (InsulatedFlasks.Contains(__instance.gameObject.name))
+            if (insulatedFlasks.Contains(__instance.gameObject.name))
             {
                 __instance.m_InsulatedFlask.m_PercentHeatLossPerMinuteIndoors = Settings.Instance.InsulatedFlaskHeatLossPerMinuteIndoors;
                 __instance.m_InsulatedFlask.m_PercentHeatLossPerMinuteOutdoors = Settings.Instance.InsulatedFlaskHeatLossPerMinuteOutdoors;
