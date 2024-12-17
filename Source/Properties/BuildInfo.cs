f0668b102087045280adfec9b287687f4ca05b4e
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-12-17 16:46:49 +1030

    v1.4.4!

--- Source/Properties/BuildInfo.cs
+++ Source/Properties/BuildInfo.cs
@@ -7,9 +7,8 @@ internal static class BuildInfo
     public const string Description = "A modification which enhances gameplay mechanics by tweaking specific elements within The Long Dark.";
     public const string Author = "Deadman";
     public const string Copyright = "Copyright © 2023 - 2024";
-    public const string Version = "1.4.4-PreRelease1";
-    //public const string AssemblyVersion = $"{Version}.0";
-    public const string AssemblyVersion = "1.4.4.0";
+    public const string Version = "1.4.4";
+    public const string AssemblyVersion = $"{Version}.0";
     public const string MelonLoaderVersion = "0.6.6";
     public const string GUIName = "Universal Tweaks";
 
