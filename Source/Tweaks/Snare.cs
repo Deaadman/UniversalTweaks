6bb9a17e9779ec7eb8d802de3bc813988da316f8
Deadman <47807486+Deaadman@users.noreply.github.com>
2025-11-25 16:20:23 +1030

    Disabled snare tweaks, added .editorconfig, added CHANGELOG.md, updated issue templates.

--- Source/Tweaks/Snare.cs
+++ Source/Tweaks/Snare.cs
@@ -1,44 +1,45 @@
-﻿namespace UniversalTweaks.Tweaks;
-
-internal static class Snare
-{
-    private static SnareItem? _snareItem;
-
-    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.InitLabelsForGear))]
-    private static class SnareItemInspectEquipPrompt
-    {
-        private static void Postfix(PlayerManager __instance)
-        {
-            _snareItem = __instance.m_Gear.m_SnareItem;
-
-            InterfaceManager.TryGetPanel<Panel_HUD>(out var hudPanel);
-            var inspectModeUILabel = hudPanel.m_InspectMode_Equip;
-
-            if (_snareItem != null && _snareItem.m_State == SnareState.WithRabbit)
-            {
-                inspectModeUILabel.gameObject.SetActive(true);
-                inspectModeUILabel.text = Localization.Get("GAMEPLAY_SetSnare");
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.UpdateInspectGear))]
-    private static class SnareItemInspectEquipFunctionality
-    {
-        private static void Postfix(PlayerManager __instance)
-        {
-            if (_snareItem == null || _snareItem.m_State != SnareState.WithRabbit)
-            {
-                return;
-            }
-
-            if (!InputManager.GetEquipPressed(__instance))
-            {
-                return;
-            }
-
-            __instance.ExitInspectGearMode(false);
-            _snareItem.SetState(SnareState.Set);
-        }
-    }
-}
+﻿//namespace UniversalTweaks.Tweaks;
+//
+//internal static class Snare
+//{
+//    private static SnareItem? _snareItem;
+//
+//    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.InitLabelsForGear))]
+//    private static class SnareItemInspectEquipPrompt
+//    {
+//        private static void Postfix(PlayerManager __instance)
+//        {
+//            _snareItem = __instance.m_Gear.m_SnareItem;
+//
+//            if (_snareItem != null && _snareItem.m_State == SnareState.WithRabbit)
+//            {
+//                InterfaceManager.TryGetPanel<Panel_HUD>(out var hudPanel);
+//                var inspectModeUILabel = hudPanel.m_InspectMode_Equip;
+//                inspectModeUILabel.gameObject.SetActive(true);
+//                inspectModeUILabel.text = Localization.Get("GAMEPLAY_SetSnare");
+//            }
+//        }
+//    }
+//
+//    [HarmonyPatch(typeof(PlayerManager), nameof(PlayerManager.UpdateInspectGear))]
+//    private static class SnareItemInspectEquipFunctionality
+//    {
+//        // TODO: Seems that this is no longer setting the snare.
+//        // Add logging to see what is happening.
+//        private static void Postfix(PlayerManager __instance)
+//        {
+//            if (_snareItem == null || _snareItem.m_State != SnareState.WithRabbit)
+//            {
+//                return;
+//            }
+//
+//            if (!InputManager.GetEquipPressed(__instance))
+//            {
+//                return;
+//            }
+//
+//            __instance.ExitInspectGearMode(false);
+//            _snareItem.SetState(SnareState.Set);
+//        }
+//    }
+//}
