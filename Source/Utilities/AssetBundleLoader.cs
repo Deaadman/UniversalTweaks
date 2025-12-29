6f095463f0af453329bc11bfca6ada815fca2fc2
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2025-12-29 01:09:34 -0400

    use workaround for il2cpp span issues

--- Source/Utilities/AssetBundleLoader.cs
+++ Source/Utilities/AssetBundleLoader.cs
@@ -2,24 +2,20 @@
 
 internal static class AssetBundleLoader
 {
-    internal static AssetBundle? LoadBundle(string path)
+    internal static AssetBundle LoadBundle(string path)
     {
-        var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
-
-        if (stream == null)
+        using (Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path))
         {
-            Logging.LogError("Stream is null. Unable to load asset bundle.");
-            return null;
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            MemoryStream? memory = new((int)stream.Length);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            stream!.CopyTo(memory);
+
+            Il2CppSystem.IO.MemoryStream memoryStream = new(memory.ToArray()); // workaround to deal with span unstripping not working properly as of 2.51
+            AssetBundle bundle = AssetBundle.LoadFromStream(memoryStream);
+
+            return bundle;
         }
-
-        var memory = new MemoryStream((int)stream.Length);
-        stream.CopyTo(memory);
-
-        var temp = AssetBundle.LoadFromMemory(memory.ToArray());
-
-        memory.Dispose();
-        stream.Dispose();
-
-        return temp;
+        ;
     }
 }
